281b7407db36b03c7a7eee64514b0525b27f7a13
Levi Smith <levi@fynx.me>
2016-08-17 15:59:15 -0500

    adding a quick remove of previous index before writing

--- blue_green_data.ps1
+++ blue_green_data.ps1
@@ -1,7 +1,9 @@
 $instanceid = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
 $indexfile = 'c:\inetpub\wwwroot\index.html'
+
 Install-WindowsFeature -Name Web-Server
 
+rm $indexfile
 
 [System.IO.File]::WriteAllLines($indexfile,'<html>')
 [System.IO.File]::AppendAllText($indexfile,'<body>' + ([Environment]::NewLine))
